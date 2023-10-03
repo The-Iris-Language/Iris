479c23d015d295c8845fa8471cbaee1e4ef38acf
Ayda Aricanli <Ayda@tufts-wifi-10-5-1-214.eecs.tufts.edu>
2023-10-03 16:16:32 -0400

    ast.ml work: classtyp, inst/class calls

--- scanner-parser/ast.ml
+++ scanner-parser/ast.ml
@@ -1,1 +1,47 @@
-(* IRIS *)
+(* ast.ml for IRIS *)
+
+type op = Add | Sub | Mult | Div | Equal | Neq | Less | Leq | Greater | Geq |
+          And | Or
+
+type uop = Neg | Not
+
+type typ = Char | Int | Bool | Float | String | Void
+
+
+type classname = string     (* a class' defined name, e.g. Object or Dog*)
+type instname = string      (* name of an INSTANCE of a class *)
+
+type classtyp = Class of classname
+
+type objenv = instname * classtyp list
+
+type bind = typ * string
+
+type expr =
+    Literal of int
+  | Fliteral of string
+  | BoolLit of bool
+  | Id of string
+  | Binop of expr * op * expr
+  | Unop of uop * expr
+  | Assign of string * expr
+  | InstCall of instname * string * expr list     (* Function call from Object Instance. can refer to self within a class *)
+  | ClassCall of classname * string * expr list   (* Class Method Function call *)
+  | Inst of classname * instname * classname * expr list (* Object inst (object type, varname, object constructor, parameters) *)
+  | Noexpr
+
+type stmt =
+    Block of stmt list
+  | Expr of expr
+  | Return of expr
+  | If of expr * stmt * stmt
+  | For of expr * expr * expr * stmt
+  | While of expr * stmt
+
+(* 
+type expr =
+  Binop of expr * operator * expr
+  | Lit of int
+  | Seq of expr * expr
+  | Asn of string * expr
+  | Var of string *)
