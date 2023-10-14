b263b365ab53c59c3839b0cc7de1d069b68568dd
Ayda Aricanli <Ayda@Aydas-MacBook.local>
2023-10-14 14:36:05 -0400

    need to langlerangle, write bash script, and comment source files before submit

--- testsyntax.sh
+++ testsyntax.sh
@@ -1,1 +1,7 @@
+#!/bin/sh
+
 # Test script for scanner and parser 
+
+# Path to the IRIS compiler.  Usually "./IRIS.native"
+# Try "_build/IRIS.native" if ocamlbuild was unable to create a symbolic link.
+IRIS="iris"
